35728f43ef01c27c266169adb20731db41b99c8f
Santos <yagosouza.santos@finastra.com>
2023-11-14 18:03:25 +0000

    Creating a temporary database

---
+++ src/Movies.Application/Infrastructure/Config/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+using Movies.Application.Repositories.Interfaces;
+
+namespace Movies.Application.Infrastructure.Config;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+        return services;
+    }
+}
