b70875a61e8dc9d3804a56c51ec0a18b57b7734f
Santos <yagosouza.santos@finastra.com>
2023-11-14 18:31:13 +0000

    Implementing movie creation

--- src/Movies.API/Controllers/MoviesController.cs
+++ src/Movies.API/Controllers/MoviesController.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories.Interfaces;
+using Movies.Contracts.Requests;
 
 namespace Movies.API.Controllers;
 
 [ApiController]
+[Route("api")]
 public class MoviesController : ControllerBase
 {
+    private readonly IMovieRepository _movieRepository;
 
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        return Created($"/api/movies/{new Guid()}", request);
+    }
 }
