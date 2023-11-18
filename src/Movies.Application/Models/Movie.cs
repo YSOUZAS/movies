2401518ad97003e299726b5bd765a28c0e71e5cb
Yago Santos <>
2023-11-18 14:29:02 +0000

    Implementing slug-based retrieval

--- src/Movies.Application/Models/Movie.cs
+++ src/Movies.Application/Models/Movie.cs
@@ -1,12 +1,28 @@
-﻿namespace Movies.Application.Models;
+﻿using System.Text.RegularExpressions;
 
-public class Movie
+namespace Movies.Application.Models;
+
+public partial class Movie
 {
     public required Guid Id { get; init; }
 
     public required string Title { get; set; }
 
+    public string Slug => GenerateSlug();
+
     public required int YearOfRelease { get; set; }
 
     public required List<string> Genres { get; set; } = new();
+
+    private string GenerateSlug()
+    {
+        var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+                                .ToLowerInvariant()
+                                .Replace(" ", "-");
+
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
+
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, matchTimeoutMilliseconds: 5)]
+    private static partial Regex SlugRegex();
 }
--- src/Movies.Contracts/Responses/MovieResponse.cs
+++ src/Movies.Contracts/Responses/MovieResponse.cs
@@ -2,17 +2,20 @@
 
 public readonly record struct MovieResponse
 {
-    public MovieResponse(Guid id, string title, int yearOfRelease, IEnumerable<string> genres)
+    public MovieResponse(Guid id, string title, string slug, int yearOfRelease, IEnumerable<string> genres)
     {
         Id = id;
         Title = title;
         YearOfRelease = yearOfRelease;
         Genres = genres;
+        Slug = slug;
     }
 
     public required Guid Id { get; init; }
 
     public required string Title { get; init; }
+
+    public required string Slug { get; init; }
 
     public required int YearOfRelease { get; init; }
 
